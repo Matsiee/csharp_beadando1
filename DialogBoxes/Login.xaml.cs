796c918bdcd5e4530ce11dbc2db4eec6e124204e
Matsiee <v.materzu@gmail.com>
2025-04-01 07:44:40 +0200

    initial commit

---
+++ DialogBoxes/Login.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Data.SQLite;
+using System.Windows;
+using System.Windows.Controls;
+using Path = System.IO.Path;
+
+namespace CsharpBeadando1;
+
+/// <summary>
+///     Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class Login : Window
+{
+    public Login()
+    {
+        InitializeComponent();
+    }
+
+    private void OnLoginButtonClick(object sender, RoutedEventArgs e)
+    {
+        string pass = "pass";
+        if (TextBox.Text == pass)
+        {
+            DialogResult = true;
+        }
+        else
+        {
+            DialogResult = false;
+            MessageBox.Show("Incorrect password");
+        }
+        Close();
+    }
+
+    private void OnCancelButtonClick(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+}
---
+++ Windows/NewPatient.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace CsharpBeadando1;
+
+public partial class NewPatient : Window
+{
+    public NewPatient()
+    {
+        InitializeComponent();
+    }
+
+    private void OnAddButtonClick(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+}
